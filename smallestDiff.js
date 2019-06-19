3780360dc452df13870d4686a153f8f76561597d
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-06-19 12:49:52 +0200

    javitas

--- smallestDiff.js
+++ smallestDiff.js
@@ -1,9 +1,11 @@
-let arr1 = [1, 2, 3, 4, 5];
-let arr2 = [6, 7, 8, 9, 10];
+let arr1 = [2, 5, 3, 4, 1];
+let arr2 = [7, 7, 8, 9, 10];
 
 const smallestDiff = (arr1, arr2) => {
   let min = Math.abs(arr1[0] - arr2[0]);
   let minTomb = [];
+  minTomb[0] = arr1[0];
+  minTomb[1] = arr2[0];
   for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
       if (Math.abs(arr1[i] - arr2[j]) < min) {
